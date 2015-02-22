0f482a456e91614b05e9ba800c76a19f7b89c265
Dlavenderstory <dshang@email.arizona.edu>
2015-02-22 14:47:28 -0700

    ggplot parameters

--- Sunday_scripts/ggplot_test.R
+++ Sunday_scripts/ggplot_test.R
@@ -20,8 +20,18 @@ df[1:2,]
 dcast(df,Species ~ variable, mean)
 
 
-myplot <-ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))
+myplot <-ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species))
 
-myplot+geom_point()
+myplot+geom_point(aes(shape = Species), size = 3)
+
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ]
+myplot2<-ggplot(data = d2, aes(carat, price,color = color))
+myplot2+geom_point(size = 2)
+
+library(MASS)
+ggplot(birthwt, aes(factor(race), bwt))+geom_boxplot()
+
+head(birthwt)
+ggplot()
 
 summary(myplot)
